57a53b6fb82b81b4f3aa1e270ca895e81a375f68
noxlock <admin@noxlock.dev>
2021-07-12 19:05:29 +1000

    remove vuex

--- MAOL/vue/src/home.js
+++ MAOL/vue/src/home.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import Vuex from "vuex";
-import storePlugin from "./vuex/vuex_store_as_plugin";
+// import Vuex from "vuex";
+// import storePlugin from "./vuex/vuex_store_as_plugin";
 import Home from './home/Home.vue'
 import vuetify from './plugins/vuetify'
 
-Vue.use(Vuex);
-Vue.use(storePlugin);
+// Vue.use(Vuex);
+// Vue.use(storePlugin);
 Vue.config.productionTip = false;
 
 new Vue({
