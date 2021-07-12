8c88c2887f01e514e68cda7ab5b99fc8919892fd
noxlock <admin@noxlock.dev>
2021-07-12 17:38:14 +1000

    change import

--- MAOL/vue/src/home/Home.vue
+++ MAOL/vue/src/home/Home.vue
@@ -1,30 +1,29 @@
 <template>
   <v-app>
     <v-main>
-        <ToolBar></ToolBar>
-        <Carousel :songs=songs></Carousel>
-        <br>
-        <br>
-        <br>
-        <ul id="example-1">
+        <h1> Test </h1>
+        <!-- <ToolBar></ToolBar>
+        <Carousel :songs=songs></Carousel> -->
+
+        <!-- <ul id="example-1">
             <li v-for="song in songs" :key="song.pk">
                 <iframe width="560" height="315" :src=song.fields.youtube_link title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </li>
-        </ul>
+        </ul> -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-import ToolBar from '../components/ToolBar.vue'
-import Carousel from '../components/Carousel.vue'
+// import ToolBar from '../components/ToolBar.vue'
+// import Carousel from '../components/Carousel.vue'
 
 
 export default {
   name: 'Home',
     components: {
-        ToolBar,
-        Carousel
+        // ToolBar,
+        // Carousel
     },
     props: {
         songs: {
