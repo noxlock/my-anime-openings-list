8eaddfd906e2c916856cde5356ffdf9d5dfde772
noxlock <admin@noxlock.dev>
2022-11-07 16:32:09 +1100

    change docker port and env keys

--- MAOL/MAOL/settings_dev.py
+++ MAOL/MAOL/settings_dev.py
@@ -2,14 +2,3 @@
 DEBUG = True
 
 ALLOWED_HOSTS = ['my-anime-openings-list.herokuapp.com', 'localhost', '127.0.0.1']
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ['DB_NAME'],
-#         'USER': os.environ['DB_USER'],
-#         'PASSWORD': os.environ['DB_PASS'],
-#         'HOST': os.environ['DB_HOST'],
-#         'PORT': os.environ['DB_PORT'],
-#     }
-# }
